1868ed9b6fc9980ea3e77f894e86cbf8d505e8e4
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-01-25 17:23:20 -0500

    Ajout des méthodes, code de base de gestion commande

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -7,8 +7,8 @@ import common.TempoKnockKnockProtocole;
 
 
 public class Serveur {
-public static void main(String[] args) throws IOException, ClassNotFoundException {
-        
+	public static void main1(String[] args) throws IOException, ClassNotFoundException 
+	{    
         if (args.length != 1) {
             System.err.println("Usage: java KnockKnockServer <port number>");
             System.exit(1);
@@ -64,4 +64,131 @@ public static void main(String[] args) throws IOException, ClassNotFoundExceptio
             System.out.println(e.getMessage());
         }
     }
+	//Tempo main pour test
+	public static void main(String[] args)
+	{
+		Serveur test = new Serveur();
+		Commande testCmd = new Commande();
+		testCmd.setCommande("compilation#chemin_relatif_du_fichier_source_1");
+		test.traiteCommande(testCmd);
+	}
+	
+	/**
+	 * prend le numéro de port, crée un SocketServer sur le port
+	 */
+	public void ApplicationServeur (int port) 
+	{
+		
+	}
+ 
+	/**
+	 * Se met en attente de connexions des clients. Suite aux connexions, elle lit
+	 * ce qui est envoyé à travers la Socket, recrée l’objet Commande envoyé par
+	 * le client, et appellera traiterCommande(Commande uneCommande)
+	 */         
+	public void aVosOrdres() 
+	{
+		
+	}
+ 
+	/**
+     * prend uneCommande dument formattée, et la traite. Dépendant du type de commande, 
+     * elle appelle la méthode spécialisée
+     */
+	public void traiteCommande(Commande uneCommande) 
+	{
+		String strCmd = uneCommande.getCommande();
+		String commandeTypeStr;
+		String[] tempoSplit = strCmd.split("#");
+		commandeTypeStr = tempoSplit[0];
+		
+		if(commandeTypeStr.compareTo("compilation")==0)
+		{
+			System.out.println("Commande compilation detecté");
+		}
+		else if(commandeTypeStr.compareTo("chargement")==0)
+		{
+			System.out.println("Commande chargement detecté");
+		}
+		else if(commandeTypeStr.compareTo("creation")==0)
+		{
+			System.out.println("Commande creation detecté");
+		}
+		else if(commandeTypeStr.compareTo("lecture")==0)
+		{
+			System.out.println("Commande lecture detecté");
+		}
+		else if(commandeTypeStr.compareTo("ecriture")==0)
+		{
+			System.out.println("Commande ecriture detecté");
+		}
+		else if(commandeTypeStr.compareTo("fonction")==0)
+		{
+			System.out.println("Commande fonction detecté");
+		}
+		else
+		{
+			System.out.println("Commande non reconnue");
+		}
+	}
+ 
+	/**
+	 * traiterLecture : traite la lecture d’un attribut. Renvoies le résultat par le 
+	 * socket
+	 */
+	public void traiterLecture(Object pointeurObjet, String attribut) 
+	{
+		
+	}
+ 
+	/**
+	 * traiterEcriture : traite l’écriture d’un attribut. Confirmes au client que l’écriture
+	 * s’est faite correctement.
+	 */
+	public void traiterEcriture(Object pointeurObjet, String attribut, Object valeur) 
+	{
+		
+	}
+
+	/**
+	 * traiterCreation : traite la création d’un objet. Confirme au client que la création
+	 * s’est faite correctement.
+	 */
+	public void traiterCreation(Class classeDeLobjet, String identificateur) 
+	{
+		
+	}
+
+	/**
+	 * traiterChargement : traite le chargement d’une classe. Confirmes au client que la création
+	 * s’est faite correctement.
+	 */
+	public void traiterChargement(String nomQualifie) 
+	{
+		
+	}
+
+	/**
+	 * traiterCompilation : traite la compilation d’un fichier source java. Confirme au client
+	 * que la compilation s’est faite correctement. Le fichier source est donné par son chemin
+	 * relatif par rapport au chemin des fichiers sources.
+	 */
+	public void traiterCompilation(String cheminRelatifFichierSource) 
+	{
+		
+	}
+
+	/**
+       * traiterAppel : traite l’appel d’une méthode, en prenant comme argument l’objet
+       * sur lequel on effectue l’appel, le nom de la fonction à appeler, un tableau de nom de 
+       * types des arguments, et un tableau d’arguments pour la fonction. Le résultat de la 
+       * fonction est renvoyé par le serveur au client (ou le message que tout s’est bien 
+       * passé)
+       **/
+	public void traiterAppel(Object pointeurObjet, String nomFonction, String[] types,Object[] valeurs) 
+	{
+	 
+	}
+
+
 }
