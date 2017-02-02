b5bc72c8cea1c053d5f6dccb1e0cf51f7c25cf19
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-02-02 16:41:49 -0500

    ServeurEnCourDeMaj

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,9 +1,17 @@
 package serveur;
+import java.lang.reflect.Field;
 import java.net.*;
+import java.util.Hashtable;
 import java.io.*;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
+
+import com.sun.corba.se.spi.activation.Server;
+import com.sun.corba.se.spi.ior.Identifiable;
+
+
+
 
 import common.Commande;
 
@@ -16,9 +24,11 @@ public class Serveur {
 	private ObjectInputStream inFromClient;
 	private ObjectOutputStream  outToClient;
 	
+	public Hashtable<String, Object> ident;
+	
 
 	//Tempo main pour test
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException
 	{
 		int port = Integer.parseInt(args[0]);
 		Serveur serveur = new Serveur(port);
@@ -41,6 +51,11 @@ public class Serveur {
 		test.traiteCommande(testCmd);
 		 */
 		serveur.traiterCompilation("./src/ca/uqac/registraire/Cours.java");
+		serveur.traiterChargement("ca.uqac.registraire.Cours");
+		serveur.traiterCreation(Class.forName("ca.uqac.registraire.Cours"), "Hello");
+		
+		System.out.println(serveur.ident.get("Hello").toString());
+		
 
 	}
 
@@ -51,6 +66,7 @@ public class Serveur {
 	public Serveur (int port) throws IOException 
 	{
 		serverSocket = new ServerSocket(port);
+		ident = new Hashtable<String,Object>();
 	}
 
 	/**
@@ -59,8 +75,10 @@ public class Serveur {
 	 * le client, et appellera traiterCommande(Commande uneCommande)
 	 * @throws IOException 
 	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */         
-	public void aVosOrdres() throws IOException, ClassNotFoundException 
+	public void aVosOrdres() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException 
 	{
 
 		Socket clientSocket = serverSocket.accept();
@@ -91,19 +109,19 @@ public class Serveur {
 	 * prend uneCommande dument formattï¿½e, et la traite. Dï¿½pendant du type de commande, 
 	 * elle appelle la mï¿½thode spï¿½cialisï¿½e
 	 * @throws IOException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public void traiteCommande(Commande uneCommande) throws IOException 
+	public void traiteCommande(Commande uneCommande) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException 
 	{
 		String strCmd = uneCommande.getCommande();
 		String commandeTypeStr;
-		String[] tempoSplit = strCmd.split("#");
-		commandeTypeStr = tempoSplit[0];
+		String[] listParam = strCmd.split("#");
 		
 		switch (uneCommande.getType()) {
 		case compilation : {
 			System.out.println("Commande compilation detectï¿½");
-			String[] listParam = strCmd.split("#");
-			
 			String[] listChemin = listParam[0].split(",");
 			for(int i = 0; i<listChemin.length;i++)
 			{
@@ -114,26 +132,38 @@ public class Serveur {
 		}
 		case chargement : {
 			System.out.println("Commande chargement detectï¿½");
+			traiterChargement(listParam[0]);
 			break;
 		}
 		case creation : {
 			System.out.println("Commande creation detectï¿½");
+			traiterCreation(Class.forName(listParam[0]), listParam[1]);
 			break;
 		}
 		case lecture : {
 			System.out.println("Commande lecture detectï¿½");
-			outToClient.writeObject(new String("Hello from serveur"));
-			System.out.println("Rï¿½ponse envoyï¿½");
+			traiterLecture(ident.get(listParam[0]), listParam[1]);
 			break;
 		}
 		case ecriture : {
 			System.out.println("Commande ecriture detectï¿½");
+			traiterEcriture(ident.get(listParam[0]), listParam[2], listParam[3]);
 			break;
 		}
 		case fonction : {
 			System.out.println("Commande fonction detectï¿½");
-			outToClient.writeObject(new String("Hello from serveur"));
-			System.out.println("Rï¿½ponse envoyï¿½");
+			String[] tabListParam = listParam[2].split(",");
+			String[] tabTypeParam = new String[listParam.length];
+			Object[] tabParam = new Object[listParam.length];
+			for(int i = 0;i<tabListParam.length;i++)
+			{
+				String[] temp = tabListParam[i].split(":");
+				tabTypeParam[i] = temp[0];
+				
+				//TODO : cast le string en fonction du type précisé 
+				tabParam[i] = temp[1];
+			}
+			traiterAppel(ident.get(listParam[0]), listParam[1], tabTypeParam, tabParam);
 			break;
 		}
 		default:
@@ -145,10 +175,27 @@ public class Serveur {
 	/**
 	 * traiterLecture : traite la lecture dï¿½un attribut. Renvoies le rï¿½sultat par le 
 	 * socket
+	 * @throws IOException 
 	 */
-	public void traiterLecture(Object pointeurObjet, String attribut) 
+	public void traiterLecture(Object pointeurObjet, String attribut) throws IOException 
 	{
-
+		Class classObject= pointeurObjet.getClass();
+		
+		try {
+			Field tempoField = classObject.getField(attribut);
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		outToClient.writeObject(new String("Hello from serveur"));
+		System.out.println("Réponse envoyé");
 	}
 
 	/**
@@ -163,19 +210,26 @@ public class Serveur {
 	/**
 	 * traiterCreation : traite la crï¿½ation dï¿½un objet. Confirme au client que la crï¿½ation
 	 * sï¿½est faite correctement.
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public void traiterCreation(Class classeDeLobjet, String identificateur) 
+	public void traiterCreation(Class classeDeLobjet, String identificateur) throws InstantiationException, IllegalAccessException 
 	{
-
+		Object tempo = classeDeLobjet.newInstance();
+		ident.put(identificateur, tempo);
 	}
 
 	/**
 	 * traiterChargement : traite le chargement dï¿½une classe. Confirmes au client que la crï¿½ation
 	 * sï¿½est faite correctement.
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public void traiterChargement(String nomQualifie) 
+	public void traiterChargement(String nomQualifie) throws ClassNotFoundException, InstantiationException, IllegalAccessException 
 	{
-
+		ClassLoader classLoad = ClassLoader.getSystemClassLoader();
+		Class tempoClass = classLoad.loadClass(nomQualifie);
 	}
 
 	/**
@@ -197,10 +251,12 @@ public class Serveur {
 	 * types des arguments, et un tableau dï¿½arguments pour la fonction. Le rï¿½sultat de la 
 	 * fonction est renvoyï¿½ par le serveur au client (ou le message que tout sï¿½est bien 
 	 * passï¿½)
+	 * @throws IOException 
 	 **/
-	public void traiterAppel(Object pointeurObjet, String nomFonction, String[] types,Object[] valeurs) 
+	public void traiterAppel(Object pointeurObjet, String nomFonction, String[] types,Object[] valeurs) throws IOException 
 	{
-
+		outToClient.writeObject(new String("Hello from serveur"));
+		System.out.println("Réponse envoyé");
 	}
 
 
